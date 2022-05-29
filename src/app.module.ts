2f4b5d87ec751592a97e5953157229ea878211f1
jiwonpg98 <jiwonpg98@gmail.com>
2022-05-29 23:50:33 +0900

    feat: module 추가 설치 및 post api 추가;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostWriteModule } from './post-write/post-write.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), PostWriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import { TypeOrmConfigService } from './typeorm/typeormConfig.service';
+import * as fs from 'fs';
+import compression from 'compression';
+import bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await makeOrmConfig();
+
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      transform: true,
+    }),
+  );
+
+  app.use(compression);
+  app.setGlobalPrefix('moae/api');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  await app.listen(process.env.PORT);
+}
+
+async function makeOrmConfig() {
+  console.log(process.env.PORT);
+  const typeOrmConfigService = new TypeOrmConfigService(process.env);
+  const typeOrmConfig = typeOrmConfigService.getTypeOrmConfig();
+
+  if (fs.existsSync('ormconfig.json')) {
+    fs.unlinkSync('ormconfig.json');
+  }
+
+  fs.writeFileSync('ormconfig.json', JSON.stringify(typeOrmConfig, null, 2));
 }
 bootstrap();
